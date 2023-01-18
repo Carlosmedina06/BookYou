f00e27d7c4c11dd797bae8dd3db901155c251dc2
DarioGabrielS <daerio@hotmail.com>
2023-01-18 20:17:56 -0300

    actions para traer la info de los books

--- client/src/redux/bookActions.js
+++ client/src/redux/bookActions.js
@@ -1,7 +1,24 @@
 import axios from 'axios'
 import { getAllBooks, getBookById } from './bookSlice'
 
-export const getBooks = ()=>(dispatch)=>{
-    
-    
+export const getBooks = ()=>async (dispatch)=>{
+
+        try {
+            const info = await axios.get('http://localhost:3001/books')
+            dispatch(getAllUsers(info.data))
+            
+        } catch (error) {
+            dispatch(userError(error))
+        }
+        
 }
+
+export const getUserId = (id)=> async (dispatch)=>{
+        try {
+            const info = await axios.get('http://localhost:3001/book/'+id)
+            dispatch(getUserById(info.data))
+        } catch (error) {
+            dispatch(userError(error))
+        }
+}    
+    
