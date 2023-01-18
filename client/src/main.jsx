208a56074743dcc581a30ddf9646985b05415298
DarioGabrielS <daerio@hotmail.com>
2023-01-18 12:05:37 -0300

    añadi Provider con store al main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
+import store from './redux/store'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <React.StrictMode>
+         <App />
+      </React.StrictMode>
+     </BrowserRouter>
+  </Provider>,
 )
