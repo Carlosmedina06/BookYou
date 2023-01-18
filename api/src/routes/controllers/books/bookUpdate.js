f86422138bcf0fd3627df7edc8768cc8c9635715
Andresrodp <arodriguezp.cs.1@gmail.com>
2023-01-17 19:24:17 -0500

    creación de la ruta para actualizar libros

--- api/src/routes/controllers/books/bookUpdate.js
+++ api/src/routes/controllers/books/bookUpdate.js
@@ -1,5 +1,15 @@
-const bookUpdate = (req, res) => {
-  res.status(200).json(`bookPut by id ${req.params.id}`)
+import Book from '../../../models/Book.js'
+const bookUpdate = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { content, description, title, img } = req.body
+
+    await Book.findByIdAndUpdate(id, { content, description, title, img })
+
+    res.status(200).json(`book ${id} modified succesful`)
+  } catch (error) {
+    next(error)
+  }
 }
 
 export default bookUpdate
