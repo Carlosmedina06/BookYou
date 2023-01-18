73c84afb2ba6c49b3c625cd9dfab301224410285
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-17 23:07:09 -0300

    update book post route with authorization

--- api/src/routes/controllers/books/bookPost.js
+++ api/src/routes/controllers/books/bookPost.js
@@ -1,21 +1,40 @@
+import jwt from 'jsonwebtoken'
+
 import Book from '../../../models/Book.js'
+import User from '../../../models/User.js'
 
 const bookPost = async (req, res) => {
   try {
     const { content, description, title, img } = req.body
+    const authorization = req.get('authorization')
 
-    if (!content || !description || !title) res.status(400).send('Missing Submit Properties')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      const token = authorization.substring(7)
+      const decodedToken = jwt.verify(token, process.env.SECRET)
 
-    const book = new Book({
-      title,
-      content,
-      description,
-      img: img || 'https://www.esstudioediciones.com/blog/escribir-libro-editorial-publicar.jpg',
-    })
+      if (!token || !decodedToken.id) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+      }
 
-    await book.save()
+      const user = await User.findById(decodedToken.id)
 
-    res.status(200).json(book)
+      if (!description || !title) res.status(400).send('Missing Submit Properties')
+
+      const book = new Book({
+        title,
+        content: content || '', // content not define yet
+        description,
+        img: img || 'https://www.esstudioediciones.com/blog/escribir-libro-editorial-publicar.jpg',
+        user: user._id,
+        comments: [],
+      })
+
+      await book.save()
+      user.books = user.books.concat(book._id)
+      await user.save()
+
+      res.status(200).json(book)
+    }
   } catch (error) {
     res.status(404).json({ error: error.message })
   }
