17dd131d2a30a8268564a50c91c2231f2f78d2fc
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-18 12:53:49 -0300

    get book by id logic

--- api/src/routes/controllers/books/getBookById.js
+++ api/src/routes/controllers/books/getBookById.js
@@ -2,7 +2,10 @@ import Book from '../../../models/Book.js'
 const getBookById = async (req, res, next) => {
   try {
     const { id } = req.params
-    const oneBook = await Book.findById(id)
+
+    const oneBook = await Book.findById(id).populate('comment')
+
+    await oneBook.populate('user', { username: 1 })
 
     res.status(200).json(oneBook)
   } catch (error) {
