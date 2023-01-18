142f765c65ba29fddb89830e9cf1c934f18b4b47
jordanvargom <jordanvargom@gmail.com>
2023-01-18 01:48:11 -0300

    creacion de las rutas getCommentsBook getCommentsUsers getAllComents

--- api/src/routes/controllers/Comments/createCommentBook.js
+++ api/src/routes/controllers/Comments/createCommentBook.js
@@ -1,5 +1,19 @@
-const createCommentBook = (req, res) => {
-  res.send('createComment')
+import Comment from '../../../models/Comment.js'
+
+const createCommentBook = async (req, res) => {
+  try {
+    const { comment, book } = req.body
+
+    if (!comment || !book) res.status(400).send('comment and book is required')
+
+    const newComment = new Comment({ comment, book })
+
+    await newComment.save()
+
+    res.status(200).send('Created book comment')
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
 }
 
 export default createCommentBook
