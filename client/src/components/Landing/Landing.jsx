e7a6b8b7047bc7791635e1e3c13f372c0f6cdd0f
88vane88 <vancig88@gmail.com>
2023-01-19 16:55:36 -0300

    no cambios

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,26 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 import { getUsers } from '../../redux/userActions'
 import { getBooks } from '../../redux/bookActions'
-import { Link } from 'react-router-dom'
-export const Landing = ()=>{
+export const Landing = () => {
+  const users = useSelector((state) => state.users.users)
 
-const users = useSelector(state=>state.users.users)
+  const books = useSelector((state) => state.books.books)
 
-const books = useSelector(state=>state.books.books)
+  const dispatch = useDispatch()
 
-const dispatch = useDispatch()
+  if (users.length < 1) dispatch(getUsers())
+  if (books.length < 1) dispatch(getBooks())
 
-if(users.length<1) dispatch(getUsers())
-if(books.length<1) dispatch(getBooks())
-
-
-    return(
-
-        <div>
-            <h1>Landing</h1>
-            <Link to='/home'>
-                <button type='button' value='enter'>Enter</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Landing</h1>
+      <Link to="/home">
+        <button type="button" value="enter">
+          Enter
+        </button>
+      </Link>
+    </div>
+  )
 }
