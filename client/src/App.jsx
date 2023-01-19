0c86b657e9475a59acf487ad60cb6441d847b416
88vane88 <vancig88@gmail.com>
2023-01-19 17:59:07 -0300

    agregue llamado al landing en app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
 
-import {Routes, Route } from 'react-router-dom'
 import Bookdetail from './components/Bookdetail/Bookdetail'
 import { DashBoard } from './components/DashBoard/dashBoard'
 import { Home } from './components/Home/home'
+import { Landing } from './components/Landing/Landing'
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route element={<Landing />} path="/" />
         <Route element={<Home />} path="/home" />
         <Route element={<DashBoard />} path="/dashBoard" />
-        <Route path='/bookdetail/:id'  element={<Bookdetail/>}/>
-       </Routes>
-
-
+        <Route element={<Bookdetail />} path="/bookdetail/:id" />
+      </Routes>
     </div>
   )
 }
