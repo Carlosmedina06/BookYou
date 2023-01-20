55bbfbc87570019fa0cf00d0f2e105ed540d4fc8
88vane88 <vancig88@gmail.com>
2023-01-20 12:47:40 -0300

    incorporacion componente card

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -4,26 +4,29 @@ import { useEffect } from 'react'
 import { getBooks, getCategorys } from '../../redux/actions/index'
 import FiltradoGenero from '../FiltradoGenero/filtradoGenero'
 import OrdAlfabetico from '../OrderAlfab/orderAlfabetico'
-
- import NavBar from '../NavBar/NavBar' 
+import NavBar from '../NavBar/NavBar'
 import { Paginated } from '../Paginated/paginated'
+import Carousel from '../CarouselRecomendados/Carousel'
+import CarouselPV from '../CarouselParaVos/Carousel'
+import CarouselN from '../CarouselNuevos/Carousel'
 
 //la variable data es hardcodeado para probar la paginacion y que la pagina no se rompa
-//puedemn borrarla y pasarle la data de los libros 
+//puedemn borrarla y pasarle la data de los libros
 const data = [
   {
-    "id": 1,
-    "title": "Post 1"
+    id: 1,
+    title: 'Post 1',
   },
   {
-    "id": 2,
-    "title": "Post 2"
+    id: 2,
+    title: 'Post 2',
   },
   {
-    "id": 3,
-    "title": "Post 3"
-  }
+    id: 3,
+    title: 'Post 3',
+  },
 ]
+
 export const Home = () => {
   const dispatch = useDispatch()
   /*   const allBooks = useSelector((state) => state.books)
@@ -36,19 +39,23 @@ export const Home = () => {
 
   return (
     <div className="grid grid-cols-12">
-    <div className=" grid col-span-3">
-    <NavBar/>
-    </div>
-    <div className="grid bg-gray col-span-9 pt-10">
-      
-      <h1>This is Home</h1>
-   
-      < div className=' place-self-center'>
-      <Paginated data={data} itemsPerPage={2}/>
+      <div className=" grid col-span-3">
+        <NavBar />
       </div>
-      <FiltradoGenero />
-      <OrdAlfabetico />
-    </div>
+      <div className="grid bg-gray col-span-9 pt-10">
+        <h1>This is Home</h1>
+        <Carousel />
+        <CarouselPV />
+        <CarouselN />
+
+        <div className=" place-self-center">
+          <Paginated data={data} itemsPerPage={2} />
+        </div>
+        <div className="text-center">
+          <FiltradoGenero />
+          <OrdAlfabetico />
+        </div>
+      </div>
     </div>
   )
 }
