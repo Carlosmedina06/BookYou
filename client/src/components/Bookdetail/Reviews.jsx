a2c16d8fdd5d2586f7b585112c91178327ca2569
juan felipe velasquez <jvelasquezmuoz@yahoo.com>
2023-01-21 10:55:25 -0500

    agregue pagina de caracteristica en construccion y trabaje en la fruncion de rate y comentarios

--- client/src/components/Bookdetail/Reviews.jsx
+++ client/src/components/Bookdetail/Reviews.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faBell,faStar} from '@fortawesome/free-solid-svg-icons'
 const Reviews = () =>{
+    const initialState = {
+    rate:'',
+     comment:''
+
+    }
+const[Review, setReview] = useState(initialState)
+
+
+const handleReview = (e) =>{
+    console.log(e.target.name)
+    console.log(e.target.value)
+setReview({
+...Review,
+[e.target.name]:e.target.value
+
+})
+console.log(Review)
+}
+
+const handleSubmitReview = (e) =>{
+e.preventDefault()
+console.log(Review)
+}
+if(Review.rate){
+    console.log(Review)
+return(
+<div>
+<form onSubmit={handleSubmitReview}>
+<textarea onChange={handleReview}  name="comment" value={Review.comment} className="border border-black" rows="5" cols="40" />
+<input type="submit" value="Enviar" />
+</form>
+</div>
+
+
+)
+
+}
+
 return(
 <div>
     <div className="grid w-48 py-15 px-25   place-content-center">
     <div className="grid grid-cols-5  content-center  w-32 flex-row my-2.5 py-15 px-25 star-wdiget">
-    
-           
         <div className="inputField" style={{color:"yellow"}}>
         <FontAwesomeIcon icon={faStar} />
-        <input  type="radio" name="rate" value="5" id="rate-5" className="input-star"/>
+        <input  onClick={handleReview} type="radio" name="rate" value="5" id="rate-5" className="input-star"/>
         <label htmlFor="rate-5"></label>
         </div>
         <div className="inputField" style={{color:"yellow"}}>
@@ -35,6 +71,7 @@ return(
         </div>
     </div>
     </div>
+
 
 
 </div>
