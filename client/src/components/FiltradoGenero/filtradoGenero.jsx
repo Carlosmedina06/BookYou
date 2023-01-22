80c88d759fd3a014896d8e03b08ee9b24b6b5fcf
88vane88 <vancig88@gmail.com>
2023-01-21 22:36:23 -0300

    correccion de error el filtrado

--- client/src/components/FiltradoGenero/filtradoGenero.jsx
+++ client/src/components/FiltradoGenero/filtradoGenero.jsx
@@ -1,23 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useState } from 'react'
-/* import { useEffect } from 'react' */
 
 import { filterCategorys } from '../../redux/actions/index'
 import style from '../FiltradoGenero/filtradoGenero.module.css'
 import Card from '../Card/Card'
 import { Paginated } from '../Paginated/paginated'
 
-export const FiltradoGenero = ({ setShowFilterGenero, showFilterGenero }) => {
+export const FiltradoGenero = ({ setShowFilterGenero, setBooks, books }) => {
   const dispatch = useDispatch()
   const [bookInput, setBookInput] = useState('todos')
 
   const allGeneros = useSelector((state) => state.category)
   let allBooks = useSelector((state) => state.books)
+  /*  const [books, setBooks] = useState(useSelector((state) => state.books)) */
 
   function handleFilterCategorys(e) {
     dispatch(filterCategorys(e.target.value))
     setBookInput(e.target.value)
-    /* console.log(e.target.value) */
+    setBooks(!books)
   }
 
   if (bookInput !== 'todos') {
@@ -26,13 +26,10 @@ export const FiltradoGenero = ({ setShowFilterGenero, showFilterGenero }) => {
     setShowFilterGenero(true)
   }
 
-  console.log(showFilterGenero)
-  /*  console.log(bookInput) */
-
   return (
     <div className={style.contenedor}>
-      <select onChange={(e) => handleFilterCategorys(e)}>
-        <option className={style.titulo} selected="selected" value="todos">
+      <select className={style.select} onChange={(e) => handleFilterCategorys(e)}>
+        <option className={style.titulo} value="todos">
           {' '}
           Géneros{' '}
         </option>
@@ -40,7 +37,7 @@ export const FiltradoGenero = ({ setShowFilterGenero, showFilterGenero }) => {
           <option key={c.id}> {c.category} </option>
         ))}
       </select>
-      <div>
+      <div className={style.cardConteiner}>
         {bookInput === 'todos' ? (
           <p />
         ) : (
