c8f658a6512ee114f734b64979234dc2d3a3779e
88vane88 <vancig88@gmail.com>
2023-01-21 22:38:28 -0300

    correccion errores filtrado alfabetico

--- client/src/components/OrderAlfab/orderAlfabetico.jsx
+++ client/src/components/OrderAlfab/orderAlfabetico.jsx
@@ -1,29 +1,30 @@
 import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import { useState } from 'react'
 
-import { getBooks, orderAlf } from '../../redux/actions/index'
+import { orderAlf } from '../../redux/actions/index'
 import style from '../OrderAlfab/orderAlf.module.css'
 
-export const OrdAlfabetico = () => {
+export const OrdAlfabetico = ({ books, setBooks }) => {
   const dispatch = useDispatch()
 
   /* const allBooks = useSelector((state) => state.books) */
 
-  useEffect(() => {
+  /*   useEffect(() => {
     dispatch(getBooks)
-  })
+  }) */
 
-  /*   const [ordAlf, setOrdAlf] = useState('') */
+  const [ordAlf, setOrdAlf] = useState('')
 
   function handleSortBooks(e) {
     e.preventDefault()
     dispatch(orderAlf(e.target.value))
-    /*   setOrdAlf(`Ordenando ${e.target.value}`) */
+    setBooks(!books)
+    setOrdAlf(`Ordenando ${e.target.value}`)
   }
 
   return (
     <div className={style.contenedor}>
-      <select onChange={(e) => handleSortBooks(e)}>
+      <select className={style.select} onChange={(e) => handleSortBooks(e)}>
         <option value="asc"> A-Z </option>
         <option value="desc"> Z-A </option>
       </select>
