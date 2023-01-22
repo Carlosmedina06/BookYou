35f4e24f7e9d315d570effa3ad8f5427754586d8
88vane88 <vancig88@gmail.com>
2023-01-22 16:42:58 -0300

    cambios para css

--- client/src/components/FiltradoGenero/filtradoGenero.jsx
+++ client/src/components/FiltradoGenero/filtradoGenero.jsx
@@ -6,43 +6,35 @@ import style from '../FiltradoGenero/filtradoGenero.module.css'
 import Card from '../Card/Card'
 import Pagination from '../Pagination/Pagination'
 
-
-export const FiltradoGenero = (
-  { setShowFilterGenero, 
-    setBooks, 
-    books,
-    bookInputtodos, 
-    setBookInputtodos,
-    setBookInput,
-    bookInput,
-    clearFilters }) => {
+export const FiltradoGenero = ({
+  setShowFilterGenero,
+  setBooks,
+  books,
+  bookInputtodos,
+  setBookInputtodos,
+  setBookInput,
+  bookInput,
+  clearFilters,
+}) => {
   const dispatch = useDispatch()
-  
 
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0)
   const [book, setBook] = useState([])
-  
 
   const allGeneros = useSelector((state) => state.category)
-  
+
   let allBooks = useSelector((state) => state.books)
   /*  const [books, setBooks] = useState(useSelector((state) => state.books)) */
 
   //seteo
 
-
-
   //seteo
-
-
 
   function handleFilterCategorys(e) {
     dispatch(filterCategorys(e.target.value))
     setBookInputtodos(e.target.value)
     setBooks(!books)
-    setBookInput("")
-    
-    
+    setBookInput('')
   }
   if (bookInputtodos !== 'todos') {
     setShowFilterGenero(false)
@@ -50,27 +42,26 @@ export const FiltradoGenero = (
     setShowFilterGenero(true)
   }
 
-  
-
-
   //data pagination-----------------------
   const totalPages = Math.ceil(allBooks.length / 5)
   const filterBooks = () => {
     const filtered = allBooks.slice(currentPage * 5, currentPage * 5 + 5)
+
     return filtered
   }
-console.log(filterBooks());
+
   //Pagina Anterior
   const prevPage = () => {
     if (currentPage > 0) setCurrentPage(currentPage - 1)
   }
   //Pagina siguiente
   const nextPage = () => {
-    if (currentPage < totalPages && allBooks.length-5 > currentPage*5) {
-        setCurrentPage(currentPage + 1)
+    if (currentPage < totalPages && allBooks.length - 5 > currentPage * 5) {
+      setCurrentPage(currentPage + 1)
     }
-}
-return (
+  }
+
+  return (
     <div className={style.contenedor}>
       <select className={style.select} onChange={(e) => handleFilterCategorys(e)}>
         <option className={style.titulo} value="todos">
@@ -87,35 +78,34 @@ return (
       </select>
 
       <div className={style.mover1}>
-         <div className={style.mover}>
+        <div className={style.mover}>
+          {bookInputtodos === 'todos' ? (
+            <p />
+          ) : (
+            filterBooks().map((book) => (
+              <Card
+                key={book.id}
+                autor={book.autor}
+                comentarios={book.content}
+                estado={book.subscription}
+                id={book.id}
+                img={book.img}
+                name={book.title}
+              />
+            ))
+          )}
+        </div>
+      </div>
+
+      <div>
         {bookInputtodos === 'todos' ? (
           <p />
         ) : (
-          filterBooks().map((book) => (
-            <Card
-              key={book.id}
-              autor={book.autor}
-              comentarios={book.content}
-              estado={book.subscription}
-              id={book.id}
-              img={book.img}
-              name={book.title}
-            />
-          ))
+          <div className={style.paginado}>
+            <Pagination nextPage={nextPage} prevPage={prevPage} totalPages={currentPage + 1} />
+          </div>
         )}
       </div>
-      </div>
-
-      <div>         
-        {bookInputtodos === 'todos' ? (           
-        <p />         
-        ) : 
-          <div className={style.paginado}>
-            <Pagination prevPage={prevPage} nextPage={nextPage} totalPages={currentPage+1} />
-          </div>       
-        }       
-        </div>
-      
     </div>
   )
 }
