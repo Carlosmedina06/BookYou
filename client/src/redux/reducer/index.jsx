337583ab8335ce165eb590a9dfe38880a34eac03
88vane88 <vancig88@gmail.com>
2023-01-25 19:11:33 -0300

    creacion search por autor

--- client/src/redux/reducer/index.jsx
+++ client/src/redux/reducer/index.jsx
@@ -1,28 +1,31 @@
-import { ERROR, GET_BOOKBY_ID, GET_USERS, GET_SEARCH_BOOK } from '../actions';
+import { ERROR, GET_BOOKBY_ID, GET_USERS } from '../actions'
 const initialState = {
   books: [],
   allBooks: [],
   detail: [],
   category: [],
+  autor: [],
   users: [],
   error: [],
-};
+}
 
 function rootReducer(state = initialState, action) {
-  let allCateg = state.allBooks;
+  let allCateg = state.allBooks
   let categFilter =
     action.payload === 'todos'
       ? allCateg
-      : allCateg.filter(c => c.category?.includes(action.payload));
+      : allCateg.filter((c) => c.category?.includes(action.payload))
 
   let bookSort =
     action.payload === 'asc'
-      ? state.books.sort((a, b) =>
-          a.title > b.title ? 1 : a.title < b.title ? -1 : 0
-        )
-      : state.books.sort((a, b) =>
-          a.title > b.title ? -1 : a.title < b.title ? 1 : 0
-        );
+      ? state.books.sort((a, b) => (a.title > b.title ? 1 : a.title < b.title ? -1 : 0))
+      : state.books.sort((a, b) => (a.title > b.title ? -1 : a.title < b.title ? 1 : 0))
+
+  /*   let allAutores = state.allBooks
+  let autorFilter =
+    action.payload === 'todos'
+      ? allAutores
+      : allAutores.filter((c) => c.author?.includes(action.payload)) */
 
   switch (action.type) {
     case 'GET_BOOKS':
@@ -30,67 +33,55 @@ function rootReducer(state = initialState, action) {
         ...state,
         books: action.payload,
         allBooks: action.payload,
-      };
+      }
     case 'GET_SEARCH_BOOK':
       return {
         ...state,
         books: action.payload,
-      };
+      }
     case 'GET_ALL_GENEROS':
       return {
         ...state,
         category: action.payload,
-      };
+      }
+    case 'GET_SEARCH_AUTORES':
+      return {
+        ...state,
+        autor: action.payload,
+      }
     case 'FILTER_CATEGORY':
       return {
         ...state,
         books: categFilter,
-      };
+      }
+    /*  case 'FILTER_AUTOR':
+      return {
+        ...state,
+        autor: autorFilter,
+      } */
     case 'ORDER_ALF':
       return {
         ...state,
         books: bookSort,
-      };
+      }
     case GET_USERS:
       return {
         ...state,
         users: action.payload,
-      };
+      }
     case ERROR:
       return {
         ...state,
         error: action.payload,
-      };
+      }
     case GET_BOOKBY_ID:
       return {
         ...state,
         detail: action.payload,
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
 
-export default rootReducer;
-
-/* export const usersSlice = createSlice({
-  name: 'users',
-  initialState: {
-    USERS: [],
-    users: [],
-    user: {},
-    error: {},
-  },
-  reducers: {
-    getAllUsers: (state, action) => {
-      state.users = action.payload
-      state.USERS = action.payload
-    },
-    getUserById: (state, action) => {
-      state.user = action.payload
-    },
-    userError: (state, action) => {
-      state.error = action.payload
-    },
-  },
-}) */
+export default rootReducer
