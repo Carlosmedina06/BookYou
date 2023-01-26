a7bcd6ed0892c2bc22c3027d14d1b3a4808d2006
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-25 22:50:19 -0300

    authprovider eliminado por momento

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { Usuario } from './components/Usuario/Usuario'
 import { CreateUser } from './components/CreateUser/CreateUser'
 import PageOnConstruction from './components/PageOnConstruction/PageOnConstruction'
 import PostBook from './components/FormBookCreate/formBook'
-import { AuthProvider } from './context/authContex'
+//import { AuthProvider } from './context/authContex'
 import MainPage from './components/Landing/index'
 import NotFound from './components/NotFound/NotFound'
 import Signup from './components/Signup/Signup'
@@ -17,25 +17,23 @@ import Login from './components/Login/Login'
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <Routes>
-          <Route element={<MainPage />} path="/" />
-          {/*         <Route element={<NavbarLayout />} path="/home" /> */}
-          <Route element={<Home />} path="/home" />
-          <Route element={<NotFound />} path="/404" />
-          <Route element={<Usuario />} path="/Usuario" />
-          <Route element={<CreateUser />} path="/createUser" />
-          <Route element={<DashBoard />} path="/dashBoard" />
-          <Route element={<Bookdetail />} path="/bookdetail/:id" />
-          <Route element={<PostBook />} path="/create/book" />
-          <Route element={<PageOnConstruction />} path="/pageonconstruction" />
-          <Route element={<PostBook />} path="createbook" />
-          <Route element={<Login />} path="/login" />
-          <Route element={<Signup />} path="/signup" />
-        </Routes>
-      </div>
-    </AuthProvider>
+    <div className="App">
+      <Routes>
+        <Route element={<MainPage />} path="/" />
+        {/*         <Route element={<NavbarLayout />} path="/home" /> */}
+        <Route element={<Home />} path="/home" />
+        <Route element={<NotFound />} path="/404" />
+        <Route element={<Usuario />} path="/Usuario" />
+        <Route element={<CreateUser />} path="/createUser" />
+        <Route element={<DashBoard />} path="/dashBoard" />
+        <Route element={<Bookdetail />} path="/bookdetail/:id" />
+        <Route element={<PostBook />} path="/create/book" />
+        <Route element={<PageOnConstruction />} path="/pageonconstruction" />
+        <Route element={<PostBook />} path="createbook" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Signup />} path="/signup" />
+      </Routes>
+    </div>
   )
 }
 
