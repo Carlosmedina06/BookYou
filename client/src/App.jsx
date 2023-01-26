cd19d23ee9c025ec3ec6adb94f327d067cd78542
juan felipe velasquez <jvelasquezmuoz@yahoo.com>
2023-01-25 22:30:55 -0500

    solucion de conflictos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,3 @@
-// import { Routes, Route } from 'react-router-dom'
-
-// import Bookdetail from './components/Bookdetail/Bookdetail'
-// import { DashBoard } from './components/DashBoard/dashBoard'
-// import { Home } from './components/Home/home'
-// import { Usuario } from './components/Usuario/Usuario'
-// import { CreateUser } from './components/CreateUser/CreateUser'
-// import PageOnConstruction from './components/PageOnConstruction/PageOnConstruction'
-// import PostBook from './components/FormBookCreate/formBook'
-// import Autentication from './components/Autentication/Autentication'
-// import { AuthProvider } from './context/authContex'
-// import MainPage from './components/Landing/index'
-// import NotFound from './components/NotFound/NotFound'
-// import MainContainerLayout from './layout/Main-container-layout'
-// import NavbarLayout from './layout/Nav.layout'
-// import MainContentLayout from './layout/main-content.layout'
-// /* import NavbarLayout from './layout/navLayout' */
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <MainContainerLayout/>
-//       <NavbarLayout/>
-//       <MainContentLayout>
-//       <div className="App">
-//         <Routes>
-//           <Route element={<MainPage />} path="/" />
-//           {/*  <Route element={<NavbarLayout />} path="/home" /> */}
-//           <Route element={<Home />} path="/home" />
-//           <Route element={<NotFound />} path="/404" />
-//           <Route element={<Usuario />} path="/Usuario" />
-//           <Route element={<CreateUser />} path="/createUser" />
-//           <Route element={<DashBoard />} path="/dashBoard" />
-//           <Route element={<Bookdetail />} path="/bookdetail/:id" />
-//           <Route element={<PostBook />} path="/create/book" />
-//           <Route element={<PageOnConstruction />} path="/pageonconstruction" />
-//           <Route element={<PostBook />} path="createbook" />
-//           <Route element={<Autentication />} path="/login" />
-//         </Routes>
-//       </div>
-//       </MainContentLayout>
-//     </AuthProvider>
-//   )
-// }
-
-// export default App
-
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -63,8 +16,6 @@ import NavbarLayout from './layout/Nav.layout'
 import MainContentLayout from './layout/main-content.layout'
 import Login from './components/Login/Login'
 
-
-/* import NavbarLayout from './layout/navLayout' */
 
 function App() {
   return (
