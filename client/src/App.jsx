f72aea4856af30e14b9dff34c455847dcb208841
juan felipe velasquez <jvelasquezmuoz@yahoo.com>
2023-01-25 22:28:38 -0500

    solucion de conflictos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,51 @@
+// import { Routes, Route } from 'react-router-dom'
+
+// import Bookdetail from './components/Bookdetail/Bookdetail'
+// import { DashBoard } from './components/DashBoard/dashBoard'
+// import { Home } from './components/Home/home'
+// import { Usuario } from './components/Usuario/Usuario'
+// import { CreateUser } from './components/CreateUser/CreateUser'
+// import PageOnConstruction from './components/PageOnConstruction/PageOnConstruction'
+// import PostBook from './components/FormBookCreate/formBook'
+// import Autentication from './components/Autentication/Autentication'
+// import { AuthProvider } from './context/authContex'
+// import MainPage from './components/Landing/index'
+// import NotFound from './components/NotFound/NotFound'
+// import MainContainerLayout from './layout/Main-container-layout'
+// import NavbarLayout from './layout/Nav.layout'
+// import MainContentLayout from './layout/main-content.layout'
+// /* import NavbarLayout from './layout/navLayout' */
+
+// function App() {
+//   return (
+//     <AuthProvider>
+//       <MainContainerLayout/>
+//       <NavbarLayout/>
+//       <MainContentLayout>
+//       <div className="App">
+//         <Routes>
+//           <Route element={<MainPage />} path="/" />
+//           {/*  <Route element={<NavbarLayout />} path="/home" /> */}
+//           <Route element={<Home />} path="/home" />
+//           <Route element={<NotFound />} path="/404" />
+//           <Route element={<Usuario />} path="/Usuario" />
+//           <Route element={<CreateUser />} path="/createUser" />
+//           <Route element={<DashBoard />} path="/dashBoard" />
+//           <Route element={<Bookdetail />} path="/bookdetail/:id" />
+//           <Route element={<PostBook />} path="/create/book" />
+//           <Route element={<PageOnConstruction />} path="/pageonconstruction" />
+//           <Route element={<PostBook />} path="createbook" />
+//           <Route element={<Autentication />} path="/login" />
+//         </Routes>
+//       </div>
+//       </MainContentLayout>
+//     </AuthProvider>
+//   )
+// }
+
+// export default App
+
+
 import { Routes, Route } from 'react-router-dom'
 
 import Bookdetail from './components/Bookdetail/Bookdetail'
@@ -7,13 +55,15 @@ import { Usuario } from './components/Usuario/Usuario'
 import { CreateUser } from './components/CreateUser/CreateUser'
 import PageOnConstruction from './components/PageOnConstruction/PageOnConstruction'
 import PostBook from './components/FormBookCreate/formBook'
-import Autentication from './components/Autentication/Autentication'
 import { AuthProvider } from './context/authContex'
 import MainPage from './components/Landing/index'
 import NotFound from './components/NotFound/NotFound'
 import MainContainerLayout from './layout/Main-container-layout'
 import NavbarLayout from './layout/Nav.layout'
 import MainContentLayout from './layout/main-content.layout'
+import Login from './components/Login/Login'
+
+
 /* import NavbarLayout from './layout/navLayout' */
 
 function App() {
@@ -35,7 +85,7 @@ function App() {
           <Route element={<PostBook />} path="/create/book" />
           <Route element={<PageOnConstruction />} path="/pageonconstruction" />
           <Route element={<PostBook />} path="createbook" />
-          <Route element={<Autentication />} path="/login" />
+          {/* <Route element={<Autentication />} path="/login" /> */}
         </Routes>
       </div>
       </MainContentLayout>
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,40 +1,51 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import style from './NavBar.module.css'
 
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import style from "./NavBar.module.css"
-import { useLocation } from 'react-router-dom';
-const NavBar = () =>{
-const handleSuscribe = (e) =>{
-e.preventDefault()
-window.open("/pageonconstruction")}
-
+const NavBar = () => {
+  const handleSuscribe = (e) => {
+    e.preventDefault()
+    window.open('/pageonconstruction')
+  }
   const location = useLocation();
 
   if (location.pathname === '/') {
     return null;
   }
-return(
-<div className = {style.NavBarContainer} >
-<div className={style.NavBarLogo}>
-  <span>
-    BOOKYOU
-  </span>
-</div >
 
-    <nav className={style.NavBarOption}>
-       <ul>
-        <li ><NavLink to="/home">Inicio</NavLink></li>
-        <li ><NavLink to="/createbook">Crear Libro</NavLink></li>
-      
+  return (
+    <div className={style.NavBarContainer}>
+      <div className={style.NavBarLogo}>
+        <span>BOOKYOU</span>
+      </div>
+
+      <nav className={style.NavBarOption}>
+        <ul>
+          <li>
+            <NavLink to="/home">Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/createbook">Crear Libro</NavLink>
+          </li>
         </ul>
-       
-    </nav>
-   <div className={style.buttonSuscribeContainer}>
-   <div> <button onClick={handleSuscribe} onClick={handleSuscribe}className={style.buttonSuscribe}>Suscribirse</button></div>
-    </div>
+      </nav>
+      <div className={style.buttonSuscribeContainer}>
+        <div>
+          {' '}
+          <Link to="/login">
+            <button
+              className={style.buttonSuscribe}
+            // onClick={handleSuscribe}
+            // onClick={handleSuscribe}
+            >
+              Suscribirse
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
-  }
+}
 
 export default NavBar
-
