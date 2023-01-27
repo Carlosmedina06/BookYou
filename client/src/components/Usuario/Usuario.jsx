da035f97e87cae8d1a06f38eabc9232d9022f3b6
JoeIt <josignaciot@gmail.com>
2023-01-27 10:48:02 -0300

    Comment added

--- client/src/components/Usuario/Usuario.jsx
+++ client/src/components/Usuario/Usuario.jsx
@@ -1,15 +1,15 @@
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { useEffect, useState } from 'react';
 
-import NavBar from '../NavBar/NavBar';
-import portada from './7442df453f2197f67249ab1b49ebb44b.jpg';
-import perfil from './271812565_2088201441327034_8800356643564968973_n.png';
-import style from './Usuario.module.css';
+// import NavBar from '../NavBar/NavBar';
+// import portada from './7442df453f2197f67249ab1b49ebb44b.jpg';
+// import perfil from './271812565_2088201441327034_8800356643564968973_n.png';
+// import style from './Usuario.module.css';
 
-import Carousel from '../CarouselRecomendados/Carousel';
-import CarouselPV from '../CarouselParaVos/Carousel';
-import CarouselN from '../CarouselNuevos/Carousel';
-import SearchBar from '../SearchBar/SearchBar';
+// import Carousel from '../CarouselRecomendados/Carousel';
+// import CarouselPV from '../CarouselParaVos/Carousel';
+// import CarouselN from '../CarouselNuevos/Carousel';
+// import SearchBar from '../SearchBar/SearchBar';
 import UserBookCard from './UserBookCard';
 
 export const Usuario = () => {
