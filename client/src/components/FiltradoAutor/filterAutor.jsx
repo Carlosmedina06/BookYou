c19362e2ece0ebb3676ef5d87dfc5cc5af9f325c
88vane88 <vancig88@gmail.com>
2023-01-26 22:17:08 -0300

    cambios para css

--- client/src/components/FiltradoAutor/filterAutor.jsx
+++ client/src/components/FiltradoAutor/filterAutor.jsx
@@ -5,21 +5,19 @@ import { useSelector } from 'react-redux'
 import Card from '../Card/Card'
 import Pagination from '../Pagination/Pagination'
 import style2 from '../FiltradoGenero/filtradoGenero.module.css'
-
 import style from '../FiltradoAutor/filterAutor.module.css'
 import CssGenerico from '../CssGenerico/CssGenerico.module.css'
 
-function SearchByAutor({ 
+function SearchByAutor({
   authorInput,
   clearFilters,
   setBookInput,
   setAuthorInput,
   setBookInputtodos,
-  setShowFilterAutor
+  setShowFilterAutor,
 }) {
   const autor = useSelector((state) => state.autor)
   const [currentPage, setCurrentPage] = useState(1)
-
 
   const handleInputChange = (e) => {
     setAuthorInput(e.target.value)
@@ -59,29 +57,27 @@ function SearchByAutor({
 
   return (
     <>
-      <div >
-        
-          <div>
-            <div className={style.svgConteiner}>
-              <svg
-                aria-hidden="true"
-                className={style.svg}
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
+      <div>
+        <div>
+          <div className={style.svgConteiner}>
+            <svg
+              aria-hidden="true"
+              className={style.svg}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              />
+            </svg>
+          </div>
 
-            <div style={{ position: 'absolute', top: '20px', left: '600px' }}>
-
+          <div style={{ position: 'absolute', top: '20px', left: '600px' }}>
             <input
               required
               className={CssGenerico.search}
@@ -89,42 +85,41 @@ function SearchByAutor({
               placeholder=" Autores"
               type="search"
               onChange={handleInputChange}
-              />
-              </div>
+            />
           </div>
+        </div>
 
-          <div className={style.mover1}>
-            <div className={style.mover}>
-              {authorInput === '' ? (
-                <p />
-              ) : filterAuthor.length > 0 ? (
-                filterBooks().map((book, i) => (
-                  <Card
-                    key={i}
-                    autor={book.autor}
-                    comentarios={book.content}
-                    estado={book.subscription}
-                    id={book.id}
-                    img={book.img}
-                    name={book.title}
-                  />
-                ))
-              ) : (
-                <p className={style.p}>No tenemos ningún autor con ese nombre</p>
-              )}
-            </div>
-          </div>
-
-          <div className=" place-self-center">
+        <div className={style.mover1}>
+          <div className={style.mover}>
             {authorInput === '' ? (
               <p />
+            ) : filterAuthor.length > 0 ? (
+              filterBooks().map((book, i) => (
+                <Card
+                  key={i}
+                  autor={book.autor}
+                  comentarios={book.content}
+                  estado={book.subscription}
+                  id={book.id}
+                  img={book.img}
+                  name={book.title}
+                />
+              ))
             ) : (
-              <div className={style.paginado}>
-                <Pagination nextPage={nextPage} prevPage={prevPage} totalPages={currentPage + 1} />
-              </div>
+              <p className={style.p}>No tenemos ningún autor con ese nombre</p>
             )}
           </div>
-        
+        </div>
+
+        <div className=" place-self-center">
+          {authorInput === '' ? (
+            <p />
+          ) : (
+            <div className={style.paginado}>
+              <Pagination nextPage={nextPage} prevPage={prevPage} totalPages={currentPage + 1} />
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
