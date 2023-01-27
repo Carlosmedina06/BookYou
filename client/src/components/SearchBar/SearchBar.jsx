da0d5b9cf0d5f34c45d7078716432837e5336d5d
Dante Sebastian Choque <dsebastianc397@gmail.com>
2023-01-27 02:23:33 -0300

    search left 10px

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -69,26 +69,8 @@ function SearchBar({
     <>
       <div >
         
-          <div>
-            <div className={style.svgConteiner}>
-              <svg
-                aria-hidden="true"
-                className={style.svg}
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
-
-            <div style={{ position: 'absolute', top: '20px', left: '300px' }}>
+          <div >
+            <div style={{ position: 'absolute', top: '20px', left: '300px', paddingRight: "300px" }}>
             <input
               required
               className={CssGenerico.search}
