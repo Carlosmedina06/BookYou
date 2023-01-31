258aa5d705de4d965d6b5220c7ba7e9fbddf41a2
88vane88 <vancig88@gmail.com>
2023-01-31 16:26:04 -0300

    vuelta a cambios como main y borre cosas que ya no iban porque estaban comentadas asi queda mas limpio todo

--- client/src/components/FiltradoAutor/filterAutor.jsx
+++ client/src/components/FiltradoAutor/filterAutor.jsx
@@ -1,8 +1,7 @@
 import style from '../FiltradoAutor/filterAutor.module.css'
 import CssGenerico from '../CssGenerico/CssGenerico.module.css'
 
-function SearchByAutor({ setAuthorInput,authorInput }) {
-
+function SearchByAutor({ setAuthorInput }) {
   const handleInputChange = (e) => {
     setAuthorInput(e.target.value)
   }
@@ -10,16 +9,35 @@ function SearchByAutor({ setAuthorInput,authorInput }) {
   return (
     <>
       <div>
-        <div style={{ position: 'absolute', top: '20px', left: '600px' }}>
-          <input
-            required
-            className={CssGenerico.search}
-            id="default-search"
-            placeholder=" Autores"
-            type="text"
-            value={authorInput}
-            onChange={handleInputChange}
-          />
+        <div>
+          <div className={style.svgConteiner}>
+            <svg
+              aria-hidden="true"
+              className={style.svg}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              />
+            </svg>
+          </div>
+
+          <div style={{ position: 'absolute', top: '20px', left: '600px' }}>
+            <input
+              required
+              className={CssGenerico.search}
+              id="default-search"
+              placeholder=" Autores"
+              type="search"
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
       </div>
     </>
