8a36af87cc6d0dbceb4e7ad1f410a382a56bc110
Dante Sebastian Choque <dsebastianc397@gmail.com>
2023-01-31 09:38:17 -0300

    boton para limpiar filtros y -mostrar ocultar1- ordenado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,58 +1,25 @@
 import CssGenerico from '../CssGenerico/CssGenerico.module.css'
-/* import style from './SearchBar.module.css' */
 
-
-function SearchBar({ setBookInput }) {
-  //seteo
-
-  //seteo
+function SearchBar({ setBookInput,bookInput }) {
 
   const handleInputChange = (e) => {
     setBookInput(e.target.value)
   }
 
-  /*   const filteredResults = books.filter((book) =>
-    book.title.toLowerCase().includes(bookInput.toLowerCase()),
-  ) */
- 
   return (
     <>
       <div>
-      
-        <div>
           <div style={{ position: 'absolute', top: '20px', left: '300px', paddingRight: '300px' }}>
             <input
               required
               className={CssGenerico.search}
               id="default-search"
               placeholder="Libros, Textos, Artículos..."
-              type="search"
+              type="text"
+              value={bookInput}
               onChange={handleInputChange}
             />
           </div>
-        </div>
-
-        {/*   <div className={style.mover1}>
-          <div className={style.mover}>
-            {bookInput === '' ? (
-              <p />
-            ) : filteredResults.length > 0 ? (
-              filterBooks().map((book, i) => (
-                <Card
-                  key={i}
-                  autor={book.autor}
-                  comentarios={book.content}
-                  estado={book.subscription}
-                  id={book.id}
-                  img={book.img}
-                  name={book.title}
-                />
-              ))
-            ) : (
-              <p className={style.p}>No tenemos ningún texto con ese nombre</p>
-            )}
-          </div>
-        </div> */}
       </div>
     </>
   )
