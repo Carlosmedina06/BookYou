debaeb691f61d519534530ce69f19fa9ed5251da
jordanvargom <jordanvargom@gmail.com>
2023-02-01 16:07:58 -0300

    pasaron cositas :)

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,7 +1,6 @@
-import axios from 'axios'
 /* ACA ESTA TODA LA CONEXION BACK Y FRONT!! */
 export const GET_USERS = 'GET_USERS'
-export const GET_USER_BY_ID ='GET_USER_BY_ID'
+export const GET_USER_BY_ID = 'GET_USER_BY_ID'
 export const GET_ONE_USER = 'GET_ONE_USER'
 
 export const ERROR = 'ERROR'
@@ -15,6 +14,7 @@ export const LOGIN = 'LOGIN'
 export const REGISTER_LOCAL = 'REGISTER_LOCAL'
 export const CLEAR_BOOK_DETAILS = 'CLEAR_BOOK_DETAIL'
 export const SUBSCRIPTION = 'SUBSCRIPTION'
+import axios from 'axios'
 import {
   GoogleAuthProvider,
   signInWithPopup,
@@ -151,14 +151,22 @@ export const getUsers = () => async (dispatch) => {
 /* -------------------- GET USUARIO POR ID ----------------- */
 export const getUserById = (id) => async (dispatch) => {
   try {
-    const info = await axios.get(`${url}/user/${id}`,{
+    const info = await axios.get(`${url}/user/${id}`, {
       headers: {
         Authorization: `Bearer ${window.localStorage.getItem('token')}`,
       },
     })
+
     return dispatch({
       type: GET_USER_BY_ID,
-
+    })
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: error.message,
+    })
+  }
+}
 /* ------------------GET USER POR ID ------------------- */
 export const getOneUser = (id) => async (dispatch) => {
   try {
