9d958432ae725465ebc3b747b5702ad00501aa3d
DarioGabrielS <daerio@hotmail.com>
2023-02-03 17:53:56 -0300

    Edicion de comentarios para el dash

--- client/src/components/DashBoard/CommentEdit/CommentCard.jsx
+++ client/src/components/DashBoard/CommentEdit/CommentCard.jsx
@@ -1,22 +1,61 @@
+
 import style from './CommentCard.module.css'
+import api from '../../../utils/axios/axios'
+export const CommentCard = ({comment}) =>{
 
-export const CommentCard = ({ comment }) => {
-  const handleDelete = (e) => {
-    console.log('delete')
+
+  const handleDelete = async ()=>{
+      
+      const info = await api.put(`/comment/delete/${comment.id}`, null, {
+          headers: {
+            authorization: `bearer ${localStorage.getItem('token')}`,
+          },
+        })
+      const response = info.data
+      console.log(response)
   }
-  const handleUpdate = () => { }
-
-  return (
-    <div className={style.container}>
-      <div>
-        <p>{comment}</p>
-      </div>
-      <button type="button" onClick={handleDelete}>
-        Delete
-      </button>
-      <button type="button" onClick={handleUpdate}>
-        Update
-      </button>
-    </div>
+  
+  const handleBan = async () =>{
+      
+      const banned = await api.get(`/user/${comment.user}`)
+      const bannedUser = banned.data
+      const dataUserForBan = {...bannedUser, strike : bannedUser.strike + 1}
+      
+      const info = await api.put(`/user/update`, 
+      dataUserForBan, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      const response = info.data
+      console.log(response)
+  }
+  const handleClear = async () =>{
+      
+      const turnReportToZero = {...comment, report:0 }
+      
+      const info = await api.put(`/comment/update/${comment.id}`,
+      turnReportToZero,{
+      headers: {
+        authorization: `bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      const response = info.data
+      console.log(response)
+  }
+  
+  return(
+      <div className={style.container} key=''>
+          <div>
+            <h1>User {comment.username}</h1>
+            <h3>Number of reports {comment.report}</h3>
+              <p>Content: {comment.comment}</p>
+              
+          </div>
+          <button type='button' onClick={handleDelete}>Delete</button>
+          <button type='button' onClick={handleBan}>Ban</button>
+          <button type='button' onClick={handleClear}>Clear</button>
+          
+      </div>  
   )
 }
