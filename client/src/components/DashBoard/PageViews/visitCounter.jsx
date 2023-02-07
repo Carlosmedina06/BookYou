85d71abbf9d3420094e2ba4e54f2918ad68dc413
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-07 12:49:34 -0300

    cambios casi finales

--- client/src/components/DashBoard/PageViews/visitCounter.jsx
+++ client/src/components/DashBoard/PageViews/visitCounter.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import style from "./visitCounter.module.css"
-import { getPageViews } from "../../../redux/actions";
-import { StyledEngineProvider } from "@mui/material";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { StyledEngineProvider } from '@mui/material'
 
-const PageViews = () =>{
-const dispatch = useDispatch()
-const googleAnalyticsPageViews = useSelector((state) => state.pageviews)
-    useEffect(()=>{
-         dispatch(getPageViews())
-    },[dispatch])
+import { getPageViews } from '../../../redux/actions'
 
+import style from './visitCounter.module.css'
 
-return(
-<div className={style.PageViewsCardContainer}>
-    <div className={style.PageViewsNumber}><p>{Object.values(googleAnalyticsPageViews)}</p></div>
- <div className={style.PageViewsCardTitle}>VISITAS</div>
-  <div className={style.PageViewsCardSubtitle}>Uiltimos mes</div>
+const PageViews = () => {
+  const dispatch = useDispatch()
+  const googleAnalyticsPageViews = useSelector((state) => state.pageviews)
 
-</div>
-)}
+  useEffect(() => {
+    dispatch(getPageViews())
+  }, [dispatch])
+
+  return (
+    <div className={style.PageViewsCardContainer}>
+      <div className={style.PageViewsNumber}>
+        <p>{Object.values(googleAnalyticsPageViews)}</p>
+      </div>
+      <div className={style.PageViewsCardTitle}>VISITAS</div>
+      <div className={style.PageViewsCardSubtitle}>Uiltimos mes</div>
+    </div>
+  )
+}
 
 export default PageViews
